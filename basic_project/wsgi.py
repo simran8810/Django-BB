16d0dbe5ca2d7e56f99113db052b1bf3c7d48457
simran8810 <simran.kaur@hindustantimes.com>
2015-06-11 14:49:58 +0530

    added wsgi file

---
+++ basic_project/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for ad_campaign project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+PROJECT_PATH = os.path.dirname(__file__)
+
+sys.path.append(PROJECT_PATH)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
